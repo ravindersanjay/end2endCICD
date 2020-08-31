adaece48f6a4c0c8d3a0f066670d63deb3c762df
ravinder <ravindersanjay@gmail.com>
2020-08-31 23:10:39 +0530

    added calc and aa.txt editeed

---
+++ src/test/Calculator.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Calculator {
+
+	
+	public void add() {
+		System.out.println("this is add");
+	}
+	
+	public void substract() {
+		System.out.println("this is substract");
+	}
+	
+	public void multiply() {
+		System.out.println("this is multiply");
+	}
+	
+	public void divid() {
+		System.out.println("this is divid");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
